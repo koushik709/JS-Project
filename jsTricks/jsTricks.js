12f1db350aebf382fd075faff02d4a7f29bf4e34
Koushik Hasan <mdkoushikhasan709@gmail.com>
2024-02-07 21:50:34 +0600

    Tips & Tricks

--- jsTricks/jsTricks.js
+++ jsTricks/jsTricks.js
@@ -48,6 +48,80 @@ document.querySelector('#fileInputAudio').addEventListener('change', (event)=>{
 
 
 
+// Sticky Notes
+
+
+
+// sticky modal
+
+const deleteSticky = document.querySelectorAll('.deleteSticky');
+const addSticky = document.querySelector('.addSticky');
+const modalbg = document.querySelector('.modalbg');
+const stickyModal = document.querySelector('.stickyModal');
+
+const CancelSticky = document.querySelector('.CancelSticky');
+const submitSticky = document.querySelector('.submitSticky');
+
+
+const bodyCard = document.querySelector('.stickyCardBody');
+const textVal = document.getElementsByName('stickyText');
+
+const stickyCard = function(text){
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('Stickycard', 'm-2', 'position-relative', 'd-inline-block', 'pt-4', 'bg-light', 'p-3', 'rounded-3');
+    cardElement.innerHTML = `
+        <div class="deleteSticky">
+            <i class="fa fa-trash text-danger position-absolute"></i>
+        </div>
+        <p class="stickyText fs-12 fw-medium pt-3">${text}</p>
+    `;
+    return cardElement;
+}
+document.querySelector('.stickyCardBody').addEventListener('click', function(event) {
+    if (event.target.closest('.deleteSticky')) {
+        const stickyCard = event.target.closest('.Stickycard');
+        if (stickyCard) {
+            
+            stickyCard.remove();
+        }
+    }
+});
+// deleteSticky.forEach((el)=>{
+//     el.addEventListener('click', (event)=>{
+//         const stickyCard = event.target.closest('.Stickycard');
+//         console.log("there is not stickyCard");
+//         if (stickyCard) {
+//             stickyCard.remove();
+//         }
+//     });
+// });
+function addRemove(){
+    modalbg.classList.contains('hidden') ? modalbg.classList.remove('hidden') : modalbg.classList.add('hidden');
+    stickyModal.classList.contains('hidden') ? stickyModal.classList.remove('hidden') : stickyModal.classList.add('hidden');
+}
+
+addSticky.addEventListener('click', ()=>{
+    addRemove();
+});
+
+CancelSticky.addEventListener('click', ()=>{
+    textVal[0].value="";
+    addRemove();
+});
+
+const sizeLe = document.querySelectorAll('.Stickycard');
+submitSticky.addEventListener('click', ()=>{
+    let inputVal = textVal[0].value;
+    if (inputVal.trim() !== '') { 
+        const cardElement = stickyCard(inputVal);
+        bodyCard.appendChild(cardElement);
+        textVal[0].value="";
+        addRemove();
+    } else {
+        alert("Please Type Something.");
+    }
+});
+
 
 
 
